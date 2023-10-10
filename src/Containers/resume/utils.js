774641938744c155f0562e255ef477949dc4c902
Abhimaurya2526 <mauryaabhishek@gmail.com>
2023-10-10 12:29:58 +0530

    second

--- src/Containers/resume/utils.js
+++ src/Containers/resume/utils.js
@@ -1,13 +1,13 @@
 export const data ={
     experience: [
         {
-            title: "Internship",
-            SubTitle: "Lucknow, India",
+            title: "MERN Stact Internship",
+            SubTitle: "Digipodium Lucknow, India",
             description:"I have experience working on the MERN stack, which stands for MongoDB, Express.js, React.js, and Node.js. In the MERN stack, MongoDB is used as the database, Express.js as the backend framework, React.js for building user interfaces on the frontend, and Node.js as the runtime environment for server-side scripting. This technology stack is popular for developing full-stack web applications"
         },
         {
             title: "Junior Java Fullstack developer",
-            SubTitle: "Gujarat, India",
+            SubTitle: "Tata Strive Gujarat, India",
             description:"Four-month training program that includes theoretical and practical knowledge of frontend technologies such as HTML, CSS, JavaScript, React, Bootstrap, as well as backend technologies including Java and Spring Boot"      
          }
     ],
